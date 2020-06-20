ae2f6cc84ced4975ed62457aa06051173152e8f5
Timothy Sharkey <Techne3@gmail.com>
2020-06-20 16:28:32 -0600

    inOrder and hammingDistance solutions

---
+++ edit/inOrder.js
@@ -0,0 +1,26 @@
+// https://edabit.com/challenge/9GcCezhbB3DxoJ6fL
+
+function isInOrder(str) {
+  let order = str.split("").sort().join("");
+  return order === str ? true : false;
+}
+
+console.log(isInOrder("bca"));
+console.log(isInOrder("321"));
+console.log(isInOrder("123"));
+
+function hammingDistance(str1, str2) {
+  //     let result = 0
+  // 	for(let i = 0; i < str1.length; i++){
+  //         if(str1[i] !== str2[i]){
+  //             result++
+  //         }
+  //     }
+  //     return result
+  // }
+
+  /// other solution
+  return str1.split("").filter((x, i) => str1[i] !== str2[i]).length;
+}
+
+console.log(hammingDistance("abcde", "bcdef"));
