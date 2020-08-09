68847b8a08300bd846efb1c90ccb247b6433210a
Timothy Sharkey <Techne3@gmail.com>
2020-08-09 13:33:53 -0600

    firstDup and trace solutions

--- codeSignal/adjacentElement.js
+++ codeSignal/adjacentElement.js
@@ -16,13 +16,14 @@ console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
 // https://app.codesignal.com/arcade/intro/level-2/bq2XnSr5kbHqpHGJC
 
 function makeArrayConsecutive2(statues) {
-  let count = 0;
+  let count = [];
   statues.sort((a, b) => a - b);
   let min = statues[0];
   let max = statues[statues.length - 1];
   for (let i = min; i < max; i++) {
+    console.log(i);
     if (statues.indexOf(i) === -1) {
-      count++;
+      count.push(i);
     }
   }
   return count;
--- codeSignal/firstDuplicate.js
+++ codeSignal/firstDuplicate.js
@@ -25,3 +25,20 @@ const FD = (b) => {
 };
 
 console.log(FD("cbcba"));
+////////////
+
+function trace(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i][i];
+  }
+  return sum;
+}
+
+console.log(
+  trace([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
