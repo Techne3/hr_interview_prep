1f4c3ef043c2a8eea7efa23494b07b8aa61dd7c3
Timothy Sharkey <Techne3@gmail.com>
2021-01-24 17:39:15 -0700

    elapsedTime solution

--- edit/LevelHard/elaspedTime.js
+++ edit/LevelHard/elaspedTime.js
@@ -6,3 +6,59 @@ function elapsedTime(sa, st) {
   return et.toString().substr(16, 8);
 }
 console.log(elapsedTime("11:00:00", "12:00:00"));
+///
+
+function possibleBonus(a, b) {
+  if (b > a && a >= b - 6) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(possibleBonus(5, 3));
+////////
+
+function calculateLosses(obj) {
+  if (Object.entries(obj).length === 0) return "Lucky you!";
+  return Object.values(obj).reduce((a, b) => a + b);
+}
+
+console.log(calculateLosses({}));
+///////////
+
+function challenge1(s) {
+  return s.slice(0, 5);
+}
+
+function challenge2(s) {
+  return s.slice(-5);
+}
+
+function challenge3(s) {
+  return [...s.slice(0)].reverse().join("");
+}
+
+function challenge4(s) {
+  return [...s.slice(-7)].filter((_, i) => !(i % 2)).join("");
+}
+
+console.log(challenge4("abcdefghijklmnopqrstuvwxyz"));
+////////
+
+function arrayOfMultiples(num, length) {
+  let result = [];
+  for (let i = 1; i <= length; i++) {
+    result.push(i * num);
+  }
+  return result;
+}
+console.log(arrayOfMultiples(7, 5));
+///
+
+function testJackpot(result) {
+  let dup = new Set(...result).size;
+ return dup === 1
+  
+}
+console.log(testJackpot(["@", "@", "@", "@"]));
