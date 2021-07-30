8b37db46ae0355e65c142173068e2e421ed7a6ad
Timothy Sharkey <Techne3@gmail.com>
2021-07-29 19:07:28 -0600

    48 leetcode solution

---
+++ leetcode/48.js
@@ -0,0 +1,13 @@
+var rotate = function (matrix) {
+  const n = matrix.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+
+  matrix.forEach((row) => (row = row.reverse()));
+};
