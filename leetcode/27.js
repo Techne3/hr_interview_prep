2798af6044f5722a21efa5f016a279c3edfbdba1
Timothy Sharkey <Techne3@gmail.com>
2021-08-14 16:55:21 -0600

    26 leetcode solution

--- leetcode/27.js
+++ leetcode/27.js
@@ -1,10 +1,13 @@
 var removeElement = function (nums, val) {
-  let index = nums.indexOf(parseInt(val));
-
-  if (index > -1) {
-    nums.splice(index, 1);
-    removeElement(nums, val);
-  } else {
-    return nums.length;
+  let newLen = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == val) {
+      continue;
+    }
+    nums[newLen] = nums[i];
+    newLen++;
   }
+  return newLen;
 };
+
+console.log(removeElement([3, 2, 2, 2, 34, 3], 3));
