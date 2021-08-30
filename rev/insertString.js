11552faa4c17e45ad160fb2015100093492b55d1
Timothy Sharkey <Techne3@gmail.com>
2021-08-29 18:59:48 -0600

    fixed rave

--- rev/insertString.js
+++ rev/insertString.js
@@ -6,6 +6,16 @@ console.log(insertString("Hello World", "lo W", 4));
 
 /////
 
+function insertS(r, s, n) {
+  for (let i = 0; i < r.length; i++) {
+    if (i == n) {
+      r = r.substring(0, i) + s + r.substring(i + 1, r.length);
+    }
+  }
+  return r;
+}
+console.log(insertS("Hello World", "lo W", 4));
+
 function getFactors(n) {
   factors = [];
   for (i = 2; i < n; i++) {
