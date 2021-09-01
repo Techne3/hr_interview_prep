beb63dc1e2a6e73fc78cfd83221adb7249f8cbd2
Timothy Sharkey <Techne3@gmail.com>
2021-09-01 15:37:07 -0600

    trap Water leetCode solution

---
+++ leetcode/trapWater.js
@@ -0,0 +1,23 @@
+var trap = function (height) {
+  if (height.length === 0) return 0;
+
+  let i = 0;
+  let j = height.length - 1;
+
+  let leftMax = 0;
+  let rightMax = 0;
+  let result = 0;
+
+  while (i <= j) {
+    if (rightMax > leftMax) {
+      if (height[i] > leftMax) leftMax = height[i];
+      else result += leftMax - height[i];
+      i++;
+    } else {
+      if (height[j] > rightMax) rightMax = height[j];
+      else result += rightMax - height[j];
+      j--;
+    }
+  }
+  return result;
+};
