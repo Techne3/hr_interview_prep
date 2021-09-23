aa267819661ffd33db3eddd2a6cf396f774aa041
Timothy Sharkey <Techne3@gmail.com>
2021-09-23 17:54:28 -0600

    new solution 367

--- leetcode/367.js
+++ leetcode/367.js
@@ -6,3 +6,17 @@ var isPerfectSquare = function (num) {
   }
   return false;
 };
+/////////////
+// other solution
+var isPerfectSquare = function (num) {
+  if (num === 1 || num === 4) return true;
+  let start = 0;
+  let end = Math.floor(num / 2);
+
+  while (start + 1 < end) {
+    const mid = Math.floor((start + end) / 2);
+    if (mid ** 2 === num) return true;
+    mid ** 2 > num ? (end = mid) : (start = mid);
+  }
+  return false;
+};
--- leetcode/longestPalindrom.js
+++ leetcode/longestPalindrom.js
@@ -1,49 +1,50 @@
+function longetsPal(s) {
+  const newString = getModifiedString(s);
+  let P = new Array(newString.length).fill(0);
 
+  let center = 0;
+  let rightBoundary = 0;
 
-function longetsPal(s){
-    const newString = getModifiedString(s)
-    let P = new Array(newString.length).fill(0)
+  for (let i = 0; i < newString.length; i++) {
+    let indexMirror = (center - i, P[indexMirror]);
+    if (i < rightBoundary) P[i] = Math.min(rightBoundary - i, P[indexMirror]);
 
-    let center =0
-    let rightBoundary = 0
+    let right = i + P[i] + 1;
+    let left = i - P[i] + 1;
 
-    for( let i=0; i < newString.length; i++){
-
-        let indexMirror = center - i , P[indexMirror]
-
-        if( i < rightBoundary) P[i] = Math.min(rightBoundary - i, P[indexMirror])
-
-        let right = i + P[i] + 1
-        let left = i - P[i] + 1
-
-         while (right < newString.length && left >= 0 && newString[left] === newString[right]){
-             P[i]++
-                 right++
-                 left--
-         }
-
-         if( i + P[i] > rightBoundary){
-             center =i
-             rightBoundary = i + P[i]
-         }
-        }
-
-        let leng = Math.max(...P)
-        let index = P.indexOf(leng)
-
-        return newStrings.substring(index-length+1, index+length).split("#").join('')
-
+    while (
+      right < newString.length &&
+      left >= 0 &&
+      newString[left] === newString[right]
+    ) {
+      P[i]++;
+      right++;
+      left--;
     }
 
-    const getModifiedString = (word ) =>{
-        let newHashWord = "#"
+    if (i + P[i] > rightBoundary) {
+      center = i;
+      rightBoundary = i + P[i];
+    }
+  }
 
-        for(let char of word ){
-            newHashWord += char
-            newHashWord +="#"
-        }
-        return newHashWord
+  let leng = Math.max(...P);
+  let index = P.indexOf(leng);
 
+  return newStrings
+    .substring(index - length + 1, index + length)
+    .split("#")
+    .join("");
 }
 
-console.log(longetsPal('babad'))
+var getModifiedString = (word) => {
+  let newHashWord = "#";
+
+  for (let char of word) {
+    newHashWord += char;
+    newHashWord += "#";
+  }
+  return newHashWord;
+};
+
+console.log(longetsPal("babad"));
