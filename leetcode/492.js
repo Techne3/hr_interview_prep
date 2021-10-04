34e25faf88e537071319d01b1232e77327b5eb1b
Timothy Sharkey <Techne3@gmail.com>
2021-10-04 17:34:58 -0600

    492 leetcode solution

---
+++ leetcode/492.js
@@ -0,0 +1,11 @@
+var constructRectangle = function (area) {
+  let width = Math.floor(Math.sqrt(area));
+
+  while (width > 0) {
+    const length = area / width;
+    if (Number.isInteger(length)) {
+      return [length, width];
+    }
+    width--;
+  }
+};
