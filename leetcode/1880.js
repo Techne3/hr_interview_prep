68794a45ee36e6287aa45252a130210dcd0235aa
Timothy Sharkey <Techne3@gmail.com>
2021-10-16 19:55:27 -0600

    new solution 1880

--- leetcode/1880.js
+++ leetcode/1880.js
@@ -36,3 +36,21 @@ var isSumEqual = function (s, t, target) {
 
   return +numVal(target) === +numVal(s) + +numVal(t);
 };
+
+console.log(isSumEqual("aab", "abb", "z"));
+////////////////
+
+var isSumEqual = function (firstWord, secondWord, targetWord) {
+  const firstValue = toInteger(firstWord);
+  const secondValue = toInteger(secondWord);
+  const targetValue = toInteger(targetWord);
+  return firstValue + secondValue === targetValue;
+};
+
+function toInteger(word) {
+  let value = 0;
+  for (let i = 0; i < word.length; i++) {
+    value = value * 10 + (word.charCodeAt(i) - 97);
+  }
+  return value;
+}
