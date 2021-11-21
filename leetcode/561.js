684c05fd1ea722f030558d160adda47d32fecaf5
Timothy Sharkey <Techne3@gmail.com>
2021-11-20 18:40:17 -0700

    264 leetcode

--- leetcode/561.js
+++ leetcode/561.js
@@ -4,12 +4,16 @@ let arrayPairSum = function (nums) {
   }); //sort the array
 
   let result = 0;
+  console.log(nums);
 
   for (let i = 0; i < nums.length; i += 2) {
+    console.log(nums[i]);
+    console.log(i);
     result += nums[i];
+    console.log(result);
   }
   return result;
 };
 
-console.log(arrayPairSum([1, 4, 3, 2]));
+// console.log(arrayPairSum([1, 4, 3, 2]));
 console.log(arrayPairSum([6, 2, 6, 5, 1, 2]));
